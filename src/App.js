2e71c4e5b8565033bafdd8fbf1433756aca41bf8
ashley.fernandes <ashley.fernandes@rksv.in>
2019-06-05 23:55:10 +0530

    added users component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment, Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
-
+import Users from "./components/users/Users";
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar title=" Github Finder" icon="fab fa-github" />
-        <h1 className="App-header">Hello App</h1>
+        <div className="container">
+          <Users />
+        </div>
       </Fragment>
     );
   }
---
+++ src/components/users/Users.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "1",
+        login: "naename",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo"
+      },
+      {
+        id: "2",
+        login: "dfnaename",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo"
+      },
+      {
+        id: "3",
+        login: "newweaename",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo"
+      },
+      {
+        id: "4",
+        login: "naenacvcvcme",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+};
+
+export default Users;
