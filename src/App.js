78d64e51122f6cbd74755ba704e0f6136fde6452
ashley.fernandes <ashley.fernandes@rksv.in>
2019-11-04 00:19:46 +0530

    add alert component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import axios from "axios";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   async componentDidMount() {
@@ -16,16 +18,42 @@ class App extends Component {
     let res = await axios.get(
       `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+    console.log(res);
     this.setState({ users: res.data, loading: false });
   }
 
+  searchUsers = async user => {
+    this.setState({ loading: true });
+    let res = await axios.get(
+      `https://api.github.com/search/users?q=${user}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 2000);
+  };
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <Fragment>
         <Navbar title=" Github Finder" icon="fab fa-github" />
-        <Search />
         <div className="container">
-          <Users users={this.state.users} loading={this.state.loading} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users users={users} loading={loading} />
         </div>
       </Fragment>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: ""
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+  };
+
   onSubmit = e => {
     e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+    }
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { clearUsers, showClear } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -28,6 +42,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           ></input>
         </form>
+        {showClear && (
+          <button onClick={clearUsers} className="btn btn-block btn-light">
+            clear
+          </button>
+        )}
       </div>
     );
   }
