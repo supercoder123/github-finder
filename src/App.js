6a84817fb1d595d2311ded80874aaa96f5f57611
ashley.fernandes <ashley.fernandes@rksv.in>
2019-11-09 19:01:43 +0530

    add react router for navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Fragment, Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -30,6 +33,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //get a single  user
+  getUser = async username => {
+    this.setState({ loading: true });
+    let res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -43,19 +55,32 @@ class App extends Component {
     const { users, loading } = this.state;
 
     return (
-      <Fragment>
-        <Navbar title=" Github Finder" icon="fab fa-github" />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users users={users} loading={loading} />
-        </div>
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Navbar title=" Github Finder" icon="fab fa-github" />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users users={users} loading={loading} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 const Navbar = ({ icon, title }) => {
   return (
     <nav className="navbar bg-primary">
@@ -7,6 +9,10 @@ const Navbar = ({ icon, title }) => {
         <i className={icon} />
         {title}
       </h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
     </nav>
   );
 };
