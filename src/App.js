96711765e608f5fad5d07152b7afb9971ac04f10
ashley.fernandes <ashley.fernandes@rksv.in>
2019-12-15 20:56:12 +0530

    add home and not found components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Fragment, useState, useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/alertState";
+import { NotFound } from "./components/pages/NotFound";
 
 const App = () => {
   // useEffect(() => {
@@ -32,18 +32,10 @@ const App = () => {
             <div className="container">
               <Alert alert={alert} />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </Fragment>
